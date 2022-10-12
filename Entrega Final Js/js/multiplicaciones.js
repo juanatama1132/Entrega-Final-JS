fd470f136f29b2349199d0edbb2d0b0c2ab7c945
Juan Cruz Atamañuk <105613450+juanatama1132@users.noreply.github.com>
2022-10-12 17:41:55 -0300

    Add files via upload

---
+++ Entrega Final Js/js/multiplicaciones.js
@@ -0,0 +1,16 @@
+Swal.fire('¡ES HORA DE MULTIPLICAR!')
+let nombre = prompt("Ingrese su nombre")
+let apellido = prompt("Ingrese su apellido")
+
+alert(`Bienvenido ${nombre} ${apellido} te estabamos esperando!`)
+
+let numero = prompt("Ingresa un numero que quieras calcular su tabla de multiplicar")
+    let fin = prompt("Ingresa hasta que numero queres calcular") 
+
+for (let i = 0; i <= fin ; i++){
+    let multiplicacion = numero * i
+    alert(`${numero} x ${i} = ${multiplicacion}`)
+}
+
+Alert(`${numero} ${fin}`)
+
